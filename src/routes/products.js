dbcdb008dd193732835c91edba9bf46fa262c733
hsattar <39243545+hsattar@users.noreply.github.com>
2021-12-15 15:14:51 +0000

    added full crud for product route

--- src/routes/products.js
+++ src/routes/products.js
@@ -21,27 +21,34 @@ productsRouter.route('/')
     }
 })
 
-productsRouter.route('/id')
+productsRouter.route('/:id')
 .get(async (req, res, next) => {
     try {
-        const product = await Product.findAll()
-        res.send(product)
+        const product = await Product.findByPk(req.params.id)
+        if (product) return res.send(product)
+        res.status(404).send('Not Found')
+    } catch (error) {
+        console.log(error)
+    }
+})
+.put(async (req, res, next) => {
+    try {
+        const updatedProduct = await Product.update(req.body, {
+            where: { id: req.params.id },
+            returning: true
+        })
+        res.send(updatedProduct[1][0])
     } catch (error) {
         console.log(error)
     }
 })
 .delete(async (req, res, next) => {
     try {
-        const product = await Product.findAll()
-        res.send(product)
-    } catch (error) {
-        console.log(error)
-    }
-})
-.post(async (req, res, next) => {
-    try {
-        const product = await Product.findAll()
-        res.send(product)
+        const result = await Product.destroy({ where: {
+            id: req.params.id
+        }})
+        if (result > 0) return res.status(204).send()
+        res.status(404).send('Not Found')
     } catch (error) {
         console.log(error)
     }
