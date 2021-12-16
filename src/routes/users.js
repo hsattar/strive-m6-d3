4f4d44556c558f7ed27324d837ec8aebed27f613
hsattar <39243545+hsattar@users.noreply.github.com>
2021-12-16 14:21:40 +0000

    added crud functionality for users and category routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -24,21 +24,29 @@ usersRouter.route('/')
 usersRouter.route('/:id')
 .get(async (req, res, next) => {
   try {
-      
+      const user = await User.findByPk(req.params.id)
+      res.send(user)
   } catch (error) {
       console.log(error)
   }  
 })
 .put(async (req, res, next) => {
     try {
-        
+        const updatedUser = await User.update(req.body, {
+            where: { id: req.params.id },
+            returning: true
+        })
+        res.send(updatedUser[1][0])
     } catch (error) {
         console.log(error)
     }  
 })
 .delete(async (req, res, next) => {
     try {
-        
+        const result = await User.destroy({
+            where: { id: req.params.id }
+        })
+        res.status(204).send()
     } catch (error) {
         console.log(error)
     }  
