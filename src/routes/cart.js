c4bbf62dd14877236699b4e9598e840c251082e9
hsattar <39243545+hsattar@users.noreply.github.com>
2021-12-17 13:23:38 +0000

    able to aggregate the data in cart table

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,12 +1,17 @@
 import { Router } from 'express'
-import { Cart } from '../db/model/index.js'
+import { Cart, Product } from '../db/model/index.js'
+import { fn, col } from 'sequelize'
 
 const cartRouter = Router()
 
 cartRouter.route('/')
 .get(async (req, res, next) => {
     try {
-        const items = await Cart.findAll()
+        const items = await Cart.findAll({
+            include: { model: Product, attributes: ['name', 'description', 'image', 'price'] },
+            attributes: ['productId', [fn('COUNT', col('product.id')), 'qty']],
+            group: ['productId', 'product.id']
+        })
         res.send(items)
     } catch (error) {
         console.log(error)
